50769996968295147d2d25d27107a3feb44cd598
✧  a u r i  ✧ <lzbd@p-u-r-i-n.local>
2022-09-01 17:35:48 +0600

    Add solution to two sum

---
+++ two_sum.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h_map = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in h_map:
+                return [h_map[diff], i]
+            h_map[n] = i
