5265af1b9a2f4c0f12616325f3c9fa313d1f63f4
Omar Faruk Riyad <omar@mixrank.com>
2022-10-11 23:19:08 +0600

    solution to problem 11

---
+++ 125. Valid Palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = ''.join(c for c in s if c.isalnum())
+        if s == '':
+            return True
+        return s == s[::-1]
+        
